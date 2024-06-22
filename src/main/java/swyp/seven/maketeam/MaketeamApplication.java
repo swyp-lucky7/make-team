a82bb249bd65ddb5da0b02d43757c41aa5b70217
Sejun Park <pkt369@naver.com>
2024-06-22 10:37:59 +0900

    init

---
+++ src/main/java/swyp/seven/maketeam/MaketeamApplication.java
@@ -0,0 +1,13 @@
+package swyp.seven.maketeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaketeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaketeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swyp/seven/maketeam/MaketeamApplicationTests.java
@@ -0,0 +1,13 @@
+package swyp.seven.maketeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaketeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
